ad0c063392096c67293522d4d0d97ac679873ad4
manjulam <manjulamurali@gmail.com>
2015-03-13 13:40:35 -0400

    First Commit
    First Commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ListSwype.Models;
+
+namespace ListSwype.Controllers
+{
+    public class UserController : ApiController
+    {
+        private listswypeEntities db = new listswypeEntities();
+
+        // GET: api/User
+        public IQueryable<user> Getusers()
+        {
+            return db.users;
+        }
+
+        // GET: api/User/5
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Getuser(int id)
+        {
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/User/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putuser(int id, user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!userExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/User
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Postuser(user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.users.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.ID }, user);
+        }
+
+        // DELETE: api/User/5
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Deleteuser(int id)
+        {
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool userExists(int id)
+        {
+            return db.users.Count(e => e.ID == id) > 0;
+        }
+    }
+}
